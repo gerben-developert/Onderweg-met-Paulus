45e0283a7d820b8436d9b35993304f4b99998f35
gerben-developert <gerben@donkeymobile.app>
2026-06-08 20:43:06 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'onderweg-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => { self.clients.claim(); });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(()=>caches.match('./index.html')))
+  );
+});
